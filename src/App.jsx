af676471fa0fd35b7078127abea2b119375dcab9
Stanislav Shopov <stasio4777@gmail.com>
2025-11-23 17:57:09 +0200

    add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   
   return (
     <>
-      
+     <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-emerald-400">
+        Tailwind is working
+      </h1>
+    </div>
     </>
   )
 }
